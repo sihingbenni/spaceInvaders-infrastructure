cc7863e8a1f584a0cb6b439ffb964622fba6400a
Benjamin Puhani <bpuhani@usfca.edu>
2025-02-28 14:49:20 -0800

    MOD create logFile

--- cli/run-smoke-test.sh
+++ cli/run-smoke-test.sh
@@ -21,45 +21,48 @@ trigger_shutdown_ec2_workflow() {
 INSTANCE_ID=$1
 GITHUB_TOKEN=$2
 
-# Install dependencies
-npm install
+# Log file
+LOG_FILE="run-smoke-test.log"
 
-# Start the application
-docker compose -f test.docker-compose.yml up -d
+{
+  # Install dependencies
+  npm install
 
-# Wait until the container space_invaders is healthy, with a maximum of 10 attempts
-attempt_counter=0
-max_attempts=60
+  # Start the application
+  docker compose -f test.docker-compose.yml up -d
 
-while [ "$(docker inspect -f '{{.State.Health.Status}}' spaceinvaders-space_invaders-1)" != "healthy" ]; do
-  if [ ${attempt_counter} -eq ${max_attempts} ]; then
-    echo "Max attempts reached, container is not healthy."
+  # Wait until the container space_invaders is healthy, with a maximum of 10 attempts
+  attempt_counter=0
+  max_attempts=60
 
-    # Trigger shutdown EC2 workflow
+  while [ "$(docker inspect -f '{{.State.Health.Status}}' spaceinvaders-space_invaders-1)" != "healthy" ]; do
+    if [ ${attempt_counter} -eq ${max_attempts} ]; then
+      echo "Max attempts reached, container is not healthy."
+
+      # Trigger shutdown EC2 workflow
+      trigger_shutdown_ec2_workflow "$INSTANCE_ID" "$GITHUB_TOKEN"
+      exit 1
+    fi
+
+    attempt_counter=$((attempt_counter+1))
+    sleep 1
+  done
+
+  # Create environment variable for the game URL
+  export GAME_URL="http://localhost:8080"
+
+  # Run tests
+  npm run test
+  TEST_EXIT_CODE=$?
+
+  # Check if tests were successful
+  if [ $TEST_EXIT_CODE -eq 0 ]; then
+    echo "Tests passed successfully."
     trigger_shutdown_ec2_workflow "$INSTANCE_ID" "$GITHUB_TOKEN"
-    exit 1
-
+    exit 0
+  else
+    echo "Tests failed with exit code $TEST_EXIT_CODE."
+    trigger_shutdown_ec2_workflow "$INSTANCE_ID" "$GITHUB_TOKEN"
+    exit $TEST_EXIT_CODE
   fi
-
-  attempt_counter=$((attempt_counter+1))
-  sleep 1
-done
-
-# Create environment variable for the game URL
-export GAME_URL="http://localhost:8080"
-
-# Run tests
-npm run test
-TEST_EXIT_CODE=$?
-
-# Check if tests were successful
-if [ $TEST_EXIT_CODE -eq 0 ]; then
-  echo "Tests passed successfully."
-
-  trigger_shutdown_ec2_workflow "$INSTANCE_ID" "$GITHUB_TOKEN"
-  exit 0
-else
-  echo "Tests failed with exit code $TEST_EXIT_CODE."
-  trigger_shutdown_ec2_workflow "$INSTANCE_ID" "$GITHUB_TOKEN"
-  exit $TEST_EXIT_CODE
-fi
+} >> $LOG_FILE 2>&1
